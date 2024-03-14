a1018a4cc8acf69124b3a677e04f88888ce11ca9
moony <moonjr98@naver.com>
2024-03-14 16:17:58 +0900

    second

--- src/main/java/com/example/demo/Demo6Application.java
+++ src/main/java/com/example/demo/Demo6Application.java
@@ -1,10 +1,14 @@
 package com.example.demo;
-
 import org.springframework.boot.SpringApplication;
+import org.springframework.ui.Model;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-@RestController
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 @SpringBootApplication
 public class Demo6Application {
 
@@ -12,14 +16,25 @@ public class Demo6Application {
 		SpringApplication.run(Demo6Application.class, args);
 	}
 		
-		@GetMapping(value = "/")
-		public String doGetHelloWorld() {
-			return "Hello World";
+//	@RequestMapping("/")
+//	public String main(Model model) {
+//		model.addAttribute("data", "hello rozy~!");
+//		return "indesdasdx.html";
+//	}
+//	@GetMapping(value = "/demo")
+//	public String doGetHelloWorldDemo() {
+//		return "Hello World (Demo)";
+//	}
+//		
+//		@GetMapping("/home")
+//		public String home() {
+//			return "content/home";
+//		}
+	
+		@GetMapping("randomname")
+		public String randomname() {
+		    return "randomname";
 		}
 	
-		@GetMapping(value = "/demo")
-		public String doGetHelloWorldDemo() {
-			return "Hello World (Demo)";
-		}
 
 }
